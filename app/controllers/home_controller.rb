85712b5a0fff1a039d51cdf75d7dd5ca40f39e4d
Ziya Gökalp <ziya93141@gmail.com>
2024-02-22 23:35:46 +0300

    homepage fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,19 @@
 class HomeController < ApplicationController
   def index
     FinnhubRuby.configure do |config|
-	 config.api_key['api_key'] = 'cnbh8q1r01qks5ivq0q0cnbh8q1r01qks5ivq0qg'
-	end
-	
-	@api = FinnhubRuby::DefaultApi.new
+			config.api_key['api_key'] = 'cnbh8q1r01qks5ivq0q0cnbh8q1r01qks5ivq0qg'
+		end
+		
+		@api = FinnhubRuby::DefaultApi.new
 
   	if params[:ticker] == ""
   		@nothing = "Hey! You Forgot To Enter A Symbol"
   	elsif params[:ticker]
-  		@stock = @api.symbol_search(params[:ticker])
-  		if !@stock 
+			@present = @api.company_profile2({symbol: params[:ticker]})
+  		@stock = @api.quote(params[:ticker])
+  		if !@present.name
   			@error = "Hey! That Stock Symbol Doesn't Exist. Please Try Again."
   		end
-
   	end
 
   end
